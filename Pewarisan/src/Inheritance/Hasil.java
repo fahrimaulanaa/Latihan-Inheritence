1cbd22f444ea55836f843a5603688974494cb35b
Fahri Maulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-03-05 21:16:26 +0700

    Add files via upload

---
+++ Pewarisan/src/Inheritance/Hasil.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Hasil {
+
+    public static void main(String[] args) {
+        Anak dina=new Anak();
+        dina.setnama("Dinaa");
+        dina.Cetak();
+    }
+
+}
+
